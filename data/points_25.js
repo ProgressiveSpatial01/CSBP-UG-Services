031736f8bbfdb3ca8ae4189ede8f66f33c4e53c8
ProgressiveSpatial01 <cameron@progressivespatial.com.au>
2026-02-12 15:26:06 +0800

    Mapbox Method

--- data/points_25.js
+++
@@ -1,1 +0,0 @@
-var json_points_25 = {"type":"FeatureCollection","name":"points_25","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"xy_coordinate_resolution":1e-07,"features":[]}
--- data/texts_26.js
+++
@@ -1,1 +0,0 @@
-var json_texts_26 = {"type":"FeatureCollection","name":"texts_26","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"xy_coordinate_resolution":1e-07,"features":[]}
